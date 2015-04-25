25eb5f1364ebb6c469b4ce6d97e6475ff8ebd77a
UserN1726 <techmarine712@gmail.com>
2015-04-25 11:27:26 +0300

    Small fix

--- cost_function.m
+++ cost_function.m
@@ -1,8 +1,8 @@
 function output = cost_function(matrix,path)
     n_sites = size(matrix,1);
-    total_time = 0;
+    total = 0;
     for i = 1:n_sites - 1
-        total_time = total_time + matrix(path(i),path(i + 1));
+        total = total + matrix(path(i),path(i + 1));
     end
-    output = total_time + matrix(path(n_sites),path(1));
+    output = total + matrix(path(n_sites),path(1));
 end
